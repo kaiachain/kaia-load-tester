18b1884b3949879bf101ee9df0bac429beb28b50
Shogo Hyodo <mmoshg8u@gmail.com>
2025-09-04 16:44:19 +0900

    Disable initial distribution

--- klayslave/main.go
+++ klayslave/main.go
@@ -101,16 +101,16 @@ func createTestAccGroupsAndPrepareContracts(cfg *config.Config, accGrp *account.
 		log.Fatalf("transfer for reservoir failed, localReservoir")
 	}
 
-	for _, token := range []string{"2", "3", "4", "5", "6", "7", "8", "9", "10"} {
-		tx = globalReservoirAccount.TransferTokenSignedTxWithGuaranteeRetry(cfg.GetGCli(), localReservoirAccount, new(big.Int).Mul(big.NewInt(1e18), big.NewInt(1e18)), token)
-		receipt, err = bind.WaitMined(context.Background(), cfg.GetGCli(), tx)
-		if err != nil {
-			log.Fatalf("receipt failed, err:%v", err.Error())
-		}
-		if receipt.Status != 1 {
-			log.Fatalf("transfer for reservoir failed, localReservoir")
-		}
-	}
+	// for _, token := range []string{"2", "3", "4", "5", "6", "7", "8", "9", "10"} {
+	// 	tx = globalReservoirAccount.TransferTokenSignedTxWithGuaranteeRetry(cfg.GetGCli(), localReservoirAccount, new(big.Int).Mul(big.NewInt(1e18), big.NewInt(1e18)), token)
+	// 	receipt, err = bind.WaitMined(context.Background(), cfg.GetGCli(), tx)
+	// 	if err != nil {
+	// 		log.Fatalf("receipt failed, err:%v", err.Error())
+	// 	}
+	// 	if receipt.Status != 1 {
+	// 		log.Fatalf("transfer for reservoir failed, localReservoir")
+	// 	}
+	// }
 
 	// 3. charge KAIA
 	if cfg.InTheTcList("transferTxTC") {
